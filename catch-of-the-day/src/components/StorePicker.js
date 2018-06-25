8abf785c26ec2a573533e755b480a377aada334f
Fahid Javid <fahidjavid@gmail.com>
2018-06-25 11:59:20 +0500

    Improved StorePicker componenet.

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 class StorePicker extends React.Component {
     render() {
-        return <p>I am the store Picker!!</p>
+        return (
+            <Fragment>
+            {/*This is a StorePicker fragment*/}
+            <form action="" className="store-selector">
+            <h2>Please Enter A Store</h2>
+            <input type="text" required placeholder="Store Name"/>
+            <button type="submit">Visit Store →</button>
+            </form>
+            </Fragment>
+        )
     }
 }
 
