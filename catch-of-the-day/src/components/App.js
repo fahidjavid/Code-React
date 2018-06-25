22a1e06fe2f6c4566d61f9c60de173789fd9f374
Fahid Javid <fahidjavid@gmail.com>
2018-06-25 14:06:37 +0500

    Passed data using componenet props.

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -1,15 +1,19 @@
 import React from "react";
 import Header from './Header'
+import Inventory from './Inventory'
+import Order from './Order'
 
 class App extends React.Component {
     render() {
         return(
             <div className="catch-of-the-day">
                 <div className="menu">
-                    <Header />
+                    <Header tagline="You are cool!" display={true}/>
+                    <Header tagline="I shouldn't be displayed!" display={false}/>
+                    <Header tagline="This should be displayed." display={true}/>
                 </div>
-                {/*<inventory />*/}
-                {/*<order />*/}
+                <Inventory />
+                <Order />
             </div>
         );
     }
