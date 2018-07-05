5ad21346668faac055b6b69fa7752e811a9f31e4
Fahid Javid <fahidjavid@gmail.com>
2018-07-05 20:37:02 +0500

    Listed smaple fishes from App's State and also status applied.

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -3,6 +3,7 @@ import Header from './Header'
 import Inventory from './Inventory'
 import Order from './Order'
 import sampleFishes from '../sample-fishes';
+import Fish from './Fish';
 
 class App extends React.Component {
 
@@ -31,6 +32,9 @@ class App extends React.Component {
             <div className="catch-of-the-day">
                 <div className="menu">
                     <Header tagline="Fresh seafood Market" />
+                    <ul className="menu-fish">
+                        {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]}/>)}
+                    </ul>
                 </div>
                 <Order />
                 <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
