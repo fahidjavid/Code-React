df1af1fb16a80433ca38e66cfeecde32258e8541
Fahid Javid <fahidjavid@gmail.com>
2018-07-05 16:42:08 +0500

    Refined 'index.js' file.

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,11 +1,8 @@
 // let's go!
 
-import React from 'react';
+import React, {Fragment} from 'react';
 import {render} from 'react-dom';
-import StorePicker from './components/StorePicker';
-import App from './components/App';
+import Router from './components/Router';
 import "./css/style.css"
 
-{/*render(<StorePicker /> ,document.querySelector('#main'));*/}
-render(<App /> ,document.querySelector('#main'));
-
+render(<Router />, document.querySelector('#main'));
